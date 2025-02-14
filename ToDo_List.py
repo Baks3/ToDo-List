85c96746cc6481c402c6bbb149bb88cc697a6fae
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-02-14 13:05:31 +0200

    modified todo list, added edit function

--- ToDo_List.py
+++ ToDo_List.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_input = input("Type add, show or exit")
+    user_input = input("Type add, show , edit or exit")
     user_input = user_input.strip()
 
     match user_input:
@@ -13,6 +13,11 @@ while True:
             for todo in todos:
                 todo = todo.title()
                 print(todo)
+        case "edit":
+            number = int(input("Number of todo to edit: "))
+            number = number -1
+            new_todo = input("Enter todo: ")
+            todos[number] = new_todo
 
         case "exit":
             break
