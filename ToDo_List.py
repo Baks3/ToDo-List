12e69be685a8ea3618c7f272cac35a5aff132b1f
tmabizela023 <tmabizela023@student.wethinkcode.co.za>
2025-02-24 10:03:03 +0200

    introduced functions to handle different functionalities

--- ToDo_List.py
+++ ToDo_List.py
@@ -1,38 +1,65 @@
-todos = []
+def read_todos():
+    with open('todos.txt', 'r') as file:
+        return file.readlines()
+
+def write_todos(todos):
+    with open('todos.txt', 'w') as file:
+        file.writelines(todos)
+
+def show_todos(todos):
+    if todos:
+        for index, todo in enumerate(todos):
+            print(f"{index + 1} - {todo.strip().title()}")
+    else:
+        print("No todos to show!")
+
 
 while True:
-    user_input = input("Type add, show , edit, complete or exit: ")
-    user_input = user_input.strip()
+    todos = read_todos()
+
+    user_input = input("Type add, show, edit, complete, or exit: ").strip()
 
     match user_input:
         case "add":
-            todo = input("Enter todo: ") + "\n"
+            todo = input("Enter todo: ").strip()
+            if todo:
+                todos.append(todo + "\n")
+                write_todos(todos)
+            else:
+                print("Todo cannot be empty.")
 
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
-
-            todos.append(todo)
-
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
         case "show":
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
-            for index,todo in enumerate(todos):
-                todo = todo.title()
-                print(f"{index+1} - {todo}")
+            show_todos(todos)
+
         case "edit":
-            number = int(input("Number of todo to edit: "))
-            number = number -1
-            new_todo = input("Enter todo: ")
-            todos[number] = new_todo
+            try:
+                number = int(input("Number of todo to edit: "))
+                if 0 < number <= len(todos):
+                    new_todo = input("Enter new todo: ").strip()
+                    if new_todo:
+                        todos[number - 1] = new_todo + "\n"
+                        write_todos(todos)
+                    else:
+                        print("New todo cannot be empty.")
+                else:
+                    print("Invalid todo number.")
+            except ValueError:
+                print("Please enter a valid number.")
+
         case "complete":
-            number = int(input("Enter number of todo to complete"))
-            todos.pop(number - 1)
+            try:
+                number = int(input("Enter number of todo to complete: "))
+                if 0 < number <= len(todos):
+                    todos.pop(number - 1)
+                    write_todos(todos)
+                else:
+                    print("Invalid todo number.")
+            except ValueError:
+                print("Please enter a valid number.")
+
         case "exit":
+            print("Bye!")
             break
 
-print("Bye!")
+        case _:
+            print("Invalid input. Please try again.")
